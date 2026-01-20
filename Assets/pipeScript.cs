e432df9ef8ae368a405a171f16f0d93bf034d4dc
ketutwira2022-debug <ketutwira2022@gmail.com>
2026-01-20 12:45:03 +0800

    MULAI PROJECT

---
+++ Assets/pipeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float nilaiRandom;
+    void Start()
+    {
+        nilaiRandom = Random.Range(-2.61f, 2.58f);
+        transform.position = new Vector2(transform.position.x, nilaiRandom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Vector2.left * 100, Time.deltaTime * 5);
+    }
+       
+}
---
+++ Assets/spawnScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float time = 0;
+    float timer = 1;
+    public GameObject pipa;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time<=0) 
+        { 
+            Instantiate(pipa, transform.position, Quaternion.identity);
+            time = timer;
+        }
+        else
+        {
+             time -= Time.deltaTime;
+        }
+
+    }
+}
